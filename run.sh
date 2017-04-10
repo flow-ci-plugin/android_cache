a6be65a63349725dded162724719811c182d65c4
KaneHui <18392888381@163.com>
2017-04-10 17:56:59 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -43,3 +43,19 @@ if [[ $FLOW_ENABLE_CACHE == 'TRUE' ]]; then
 else
   echo 'cache disabled'
 fi
+
+
+if [[ $FLOW_ENABLE_CACHE == 'TRUE' ]]; then
+  echo 'start nodejs cache'
+  FLOW_CACHE_PATH="$CACHE_BASE_URL/$FLOW_VERSION/node_modules"
+  if [[ -d $FLOW_CACHE_PATH ]]; then
+    echo "cache exists"
+  else
+    echo "cache not exists"
+    mkdir -p $FLOW_CACHE_PATH
+  fi
+
+  ln -s $FLOW_CACHE_PATH $FLOW_CURRENT_PROJECT_PATH
+else
+  echo 'cache disabled'
+fi
